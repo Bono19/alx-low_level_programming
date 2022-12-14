f2b70e0c81ce12f0ce23bccf83a08209e18332c8
Bono19 <biniyame4@gmail.com>
2022-12-14 05:56:17 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+*/                                                int main(void)                                    {
+        printf("\"Programming is like building a multilingual puzzle");
+        return (0);
